cb14ae38bda7a5ca8d5631f806c7e4a1ae6d5c77
Frank Kloeker <f.kloeker@telekom.de>
2018-02-04 01:27:15 +0100

    add text file support

--- i18njson-translate.py
+++ i18njson-translate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import io
 import click
 import polib
 from collections import OrderedDict
@@ -30,8 +31,8 @@ def call_api(message, lang, to, apikey):
     " Use one of Google Translate languages codes. Example: 'en'")
 @click.option('--to', help='Target language should be in result file')
 @click.option('--apikey', help='Use API with key')
-@click.option('--ftype', help='Accept input file in json or po format',
-    type=click.Choice(['json', 'po']))
+@click.option('--ftype', help='Accept input file in json, po, or text format',
+    type=click.Choice(['json', 'po', 'text']))
 def process(inputfile, output, lang, to, apikey, ftype):
     """ Process and translate .po or JSON files and generate translated .po
     file in result. Can work with exist .po files and if in initial file
@@ -41,53 +42,63 @@ def process(inputfile, output, lang, to, apikey, ftype):
 
     """
 
-    if ftype == 'json':
-        items = json.load(open(inputfile), object_pairs_hook=OrderedDict)
+    if ftype == 'text':
+        with open(inputfile) as fp:  
+            for line in fp:
+                entries = line.split('\r\r')
+
+        with io.open(output, 'w', encoding='utf-8') as f:
+            for entry in entries:
+                translated = call_api(entry, lang, to, apikey)
+                f.write(translated)
     else:
-        items = OrderedDict()
-        ifile = polib.pofile(inputfile)
-        for entry in ifile:
-            items[entry.msgid] = entry.msgstr
-
-
-    created = False
-    if os.path.exists(output):
-        po = polib.pofile(output)
-    else:
-        po = polib.POFile()
-        created = True
-        po.metadata = {
-            'Project-Id-Version': '1.0',
-            'Report-Msgid-Bugs-To': 'you@example.com',
-            'POT-Creation-Date': '2007-10-18 14:00+0100',
-            'PO-Revision-Date': '2007-10-18 14:00+0100',
-            'Last-Translator': 'you <you@example.com>',
-            'Language-Team': 'English <yourteam@example.com>',
-            'MIME-Version': '1.0',
-            'Content-Type': 'text/plain; charset=utf-8',
-            'Content-Transfer-Encoding': '8bit',
-        }
-
-    for k, v in items.items():
-        if apikey:
-            translated = call_api(k, lang, to, apikey)
-        entry = po.find(k, by="msgid", include_obsolete_entries=True)
-        if entry:
-            entry.msgstr = translated
-            entry.obsolete = False  # mark not obsolete anymore
+        if ftype == 'json':
+            items = json.load(open(inputfile), object_pairs_hook=OrderedDict)
         else:
-            entry = polib.POEntry(msgid=k, msgstr=translated)
-            po.append(entry)
+            items = OrderedDict()
+            ifile = polib.pofile(inputfile)
+            for entry in ifile:
+                items[entry.msgid] = entry.msgstr
 
-    for entry in po:
-        if not entry.msgid in items:
-            click.echo("msgid '{}' marked as obsolete".format(entry.msgid))
-            entry.obsolete = True
 
-    if created:
-        po.save(fpath=output)
-    else:
-        po.save()
+        created = False
+        if os.path.exists(output):
+            po = polib.pofile(output)
+        else:
+            po = polib.POFile()
+            created = True
+            po.metadata = {
+                'Project-Id-Version': '1.0',
+                'Report-Msgid-Bugs-To': 'you@example.com',
+                'POT-Creation-Date': '2007-10-18 14:00+0100',
+                'PO-Revision-Date': '2007-10-18 14:00+0100',
+                'Last-Translator': 'you <you@example.com>',
+                'Language-Team': 'English <yourteam@example.com>',
+                'MIME-Version': '1.0',
+                'Content-Type': 'text/plain; charset=utf-8',
+                'Content-Transfer-Encoding': '8bit',
+            }
+
+        for k, v in items.items():
+            if apikey:
+                translated = call_api(k, lang, to, apikey)
+            entry = po.find(k, by="msgid", include_obsolete_entries=True)
+            if entry:
+                entry.msgstr = translated
+                entry.obsolete = False  # mark not obsolete anymore
+            else:
+                entry = polib.POEntry(msgid=k, msgstr=translated)
+                po.append(entry)
+
+        for entry in po:
+            if not entry.msgid in items:
+                click.echo("msgid '{}' marked as obsolete".format(entry.msgid))
+                entry.obsolete = True
+
+        if created:
+            po.save(fpath=output)
+        else:
+            po.save()
 
 
 if __name__ == '__main__':
